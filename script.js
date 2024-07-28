f1e59e7ce1f289a481f0ffc6e5cb4277e8cc6f40
picasso139000 <83409007+picasso139000@users.noreply.github.com>
2024-07-28 19:22:24 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,216 @@
+// Typed.js
+// let typed = new Typed('#element' , {
+//     strings:['<h1>Hello my name is Mahmoud Ahmed</h1>' , '<h1>I am a Developer</h1>'],
+//     typeSpeed:40,
+// })
+
+
+// Scroll Reveal
+// ScrollReveal().reveal('.demo')
+// ScrollReveal().reveal('.modi' , {delay:1000})
+
+// Animate CSS
+
+// Github
+// Freelancer
+
+// 1- Objects
+    // var empolyees = {
+    //     name : 'Hossam',
+    //     age:20,
+    //     salary:1000,
+    // }
+
+// 2- JSON
+    // JSON => JavaScript Object Notation
+    // Server Side vs Client Side
+    // JSON Object vs JS Object
+
+    
+    // Syntax :-
+    //     // 1- key : value
+    //     // 2- key take single quotes ""
+    //     // 3- Data Types => Numbers - Strings - Boolean - Null - Object - Arrays
+    //     // 4- ''
+
+    // var demo = '{"name" : "Mahmoud" , "age" : 10}';
+    // console.log(typeof demo);
+    // console.log(demo);
+    // console.log('********************');
+
+    // var jsonToObject = JSON.parse(demo);
+    // console.log(typeof jsonToObject);
+    // jsonToObject.name = 'Hossam';
+    // console.log(jsonToObject);
+    // console.log('********************');
+
+    // var stringToJSON = JSON.stringify(jsonToObject);
+    // console.log(typeof stringToJSON);
+    // console.log(stringToJSON);
+
+    // - JSON.parse()
+    // - JSON.stringify()
+    
+// 3- try..catch
+// 4- DOM
+
+// 5- BOM => Browser Object Model
+
+    // a) location
+        // window.location.href returns the href (URL) of the current page
+        // window.location.hostname returns the domain name of the web host
+        // window.location.pathname returns the path and filename of the current page
+        // window.location.protocol returns the web protocol used (http: or https:)
+        // window.location.assign('website') loads a new document
+
+    // b) Screen
+        // screen.width
+        // screen.height
+        // screen.availWidth
+        // screen.availHeight
+        // screen.colorDepth
+        // screen.pixelDepth
+
+    // c) History
+        // history.back() - same as clicking back in the browser
+        // history.forward() - same as clicking forward in the browser
+
+    // d) Timing
+        // setTimeOut
+        // setInterval
+
+            // var dem = setInterval(function(){
+            //     var x = document.createElement('h1');
+            //     var y = document.createTextNode('Holla');
+            //     x.appendChild(y);
+            //     document.body.appendChild(x);
+            // } , 1000);
+
+            // document.querySelector('input').addEventListener('click' , function(){
+            //     clearInterval(dem);
+            // })
+
+    // e) Scroll
+        // window.scrollTo(x, y);
+        // window.addEventListener('scroll' , function(){
+        //     if(scrollY >= 3000){
+        //         document.body.style.backgroundColor = 'black';
+        //     }else{
+        //         document.body.style.backgroundColor = 'white';
+        //     }
+        // })
+
+    // 6- Arrays
+
+    // 7- Math
+            // Math.round(x)	
+            // Math.ceil(x)	
+            // Math.floor(x)	
+            // Math.trunc(x)	 
+            // Math.pow()
+
+    // 8- localStorage
+        // localStorage.setItem('username', 'JohnDoe');
+        // localStorage.getItem('username');
+        // localStorage.removeItem('username');
+        // localStorage.clear();
+
+        // 2015 ES6 => EcmaScript
+
+    
+    // 9- var vs let and const
+    // 10- Regular Functions vs Anonymous Functions vs Arrow Functions
+
+    // function getName(){
+    //     console.log("Mahmoud Ahmed");
+    // }
+
+    // getName()
+
+    // let getName = function(){
+    //     console.log("Mahmoud Ahmed");
+    // }
+
+    // getName()
+
+
+    // 11- Higer Order Functions
+    // 12- github
+    // 13- Front-End Mentor
+
+
+
+    // Next Session => OOP - API - sync vs async - async vs await - Promise - Date - Modules - Validations(Security) 
+    // Hoisting - Scope - Closure
+    
+    // => Projects (Take 5 Projects Assignments + make Projects [Shopping Cart - Paralax Project - Calculator - to do List])
+
+
+
+
+
+
+    
+
+
+
+/*
+    // 1- document introduction
+    //     - DOM => Document Object Model
+    //     - هى عبارة عن مصطلح يتعلق بكل مكونات صفحة الويب من تغيير و تعديل و إضافة او حذف
+    // 2-  DOM Tree
+    //     - هى عبارة عن تفرعات شجرية او تسلسلات شجرية تُشير الى مكون صفحة الاتش تي ام ال
+
+    // 3- document elements children
+    //     1) document.documentElement
+    //     2) document.body
+    //     3) document.head
+
+    //     4) document.body.children   (only elements)
+    //     5) document.body.childNodes  (anything)
+    //     6) document.body.firstChild
+    //     7) document.body.lastChild
+
+    // 4- document elements only and siblings
+    //     1) document.body.firstElementChild
+    //     2) document.body.lastElementChild
+
+    //     3) document.body.previousElementSibling
+    //     4) document.body.nextElementSibling
+
+
+    // 6- document getElementById
+    // 7- document getElementsByName and tagName 
+    // 8- document getElementByClassName    
+
+    // 9- document innerHtml (provide description)    
+    // 10- document outerHTML
+
+    // 11- Changing style || document.getElementById('demo').style.backgroundColor = 'green';
+
+    // 12- changing Attributes || document.getElementById(id).attribute = new value
+    
+   
+    // 13- Create Element || document.createElement(element)	
+    // 14- add Content to Element || document.appendChild(element)	*/
+    // 15- addEventListener
+
+
+
+
+//     var input = document.getElementsByTagName('input')[0].value;
+//     var btn = document.getElementsByTagName('input')[1];
+
+
+// btn.addEventListener('click' , function(){
+//     var input = document.getElementsByTagName('input')[0].value;
+//     var element = document.createElement('h1');
+    
+//     element.innerHTML = input
+
+    
+// })
+
+
+
+
